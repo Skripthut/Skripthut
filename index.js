3dc2810b5dea9462962064bd915208c06168544d
TheRealUnderscore <TheRealUnderscore16@gmail.com>
2021-07-01 22:57:40 -0700

    Test

--- index.js
+++ index.js
@@ -856,6 +856,7 @@ client.on('ready', async () => {
 				break;
 
 			case 'test':
+				console.log('convert', convertBitsToBitField(7));
 				reply(interaction, 'hello', convertBitsToBitField(7));
 				break;
 
