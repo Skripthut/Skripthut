9df64e197f4150c7a0773d88a3351b07dec3d5a6
TheRealUnderscore <TheRealUnderscore16@gmail.com>
2021-09-17 09:23:19 -0700

    Fix module requirements

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ console.log(shuffle([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ]));
 try {
 	require(`dotenv`).config({ path: './secrets/client.env/' });
 } catch(error) {}
-const axios = require(`axios`);
 /**
  * @module Discord
 **/
